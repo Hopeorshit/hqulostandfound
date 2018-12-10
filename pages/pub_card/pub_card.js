4a365bde34c8a9cf29f11fc8d39e4bb84092e3b1
Hopeorshit <760263213@qq.com>
2018-12-10 23:25:13 +0800

    临时转移

--- pages/pub_card/pub_card.js
+++ pages/pub_card/pub_card.js
@@ -32,6 +32,8 @@ Page({
       student_id: null,
       institute: null,
       localImage: null,
+      is_found:true,
+      tips1:"发布您的学生卡,系统将在第一时间给您发送服务通知!"
     })
     this._initRadio();
   },
@@ -257,6 +259,63 @@ Page({
         title: '检测完成',
       })
     }
-  }
+  },
 
+  /**
+   * 选择类型
+   */
+  typeSelect: function () {
+    this.setData({
+      is_found: !this.data.is_found,
+    })
+  },
+   
+  /**
+   * 提交
+   */ 
+  onListenSubmit: function (e) {
+    let form_id = e.detail.formId;
+    let student_id = e.detail.value.student_id;
+    if (this._checkListenSubmit(student_id)) {
+      wx.showModal({
+        title: '是否确认',
+        content: '您的填写的学号是:' + student_id,
+        confirmColor: '#ff6263',
+        success: function (res) {
+          if (res.confirm) {
+            http.userBind(form_id, student_id, (res) => {
+              wx.showModal({
+                title: '提交成功',
+                content: '监听成功！由于微信formID有效期为7天,超出时间您只需要在本页面再次监听即可。',
+                confirmText: '我知道了',
+                confirmColor: '#ff6263',
+                showCancel: false
+              })
+            })
+          }
+        }
+      })
+    }
+  },
+  /**
+   * 检测学号输入
+   */
+  _checkListenSubmit: function (student_id) {
+    if (!student_id) {
+      wx.showToast({
+        title: '请填写学号',
+        icon: 'none'
+      })
+      return false;
+    }
+    return true;
+  },
+  /**
+   * 了解扫描发布页面
+   */
+  onPub_card: function () {
+    wx.navigateTo({
+      url: '/pages/pub_card/pub_card',
+    })
+  }
 })
